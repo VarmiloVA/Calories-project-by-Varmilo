c6894478d09357a955706c3ba9dbe18726200008
Varmilo <vamrilo.blue@protonmail.com>
2022-07-08 00:47:29 +0200

    Some progress made, but still not working.

--- db.sql
+++ db.sql
@@ -12,25 +12,6 @@ insert into calories(name, calories) values
 	('Pizza', 500),
     ('Ice Cream', 300),
     ('CocaCola', 80);
-
-create table user (
-    id int not null auto_increment,
-    username varchar(255) not null,
-    email varchar(255) not null,
-    age int not null,
-    primary key(id)
-);
-insert into user(username, email, age) values
-	('VarmiloVA', 'varmilo.blue@protonmail.com', 15),
-    ('Psylo', 'psylo@outlook.com', 31);
-
-create table credential (
-    id int not null auto_increment,
-    password varchar(255) not null,
-    user int,
-    foreign key (user) references user(id)
-);
-
+caloriesname
 show tables;
 select * from calories;
-select * from user;
--- main.py
+++ main.py
@@ -1,98 +1,35 @@
+from types import NoneType
 import pymysql
-from pymysql.err import OperationalError
-from tkinter import Tk, Canvas, Label, Frame, Entry, Button, W,  E, Listbox, END
+from tkinter import *
+
+from requests import request
 
 class NutritionalValues():
-    def __init__(self, login_status): # Initialize the class
-        self.login_status = login_status
-        self.stop = False
-
-    def connect(self): # Connect to the database
-        self.conn = pymysql.connect(
-            host='localhost', 
-            user='root', 
-            password='mondayl#tm#brok#n', 
-            db='nutritional_values'
-            )
-
-        self.cursor = self.conn.cursor()
-
-    def excecute(self, connection, request): # Execute a request
-        self.cursor.execute(request)
-        self.conn.commit()
+    def __init__(self, window): # Initialize the class
+        self.window = window
     
-    def run(self): # Module that runs the program
-        while self.login != False:
-            signedin = input('Are you signed in? (S/N): ')
-            if signedin.strip().upper() == "S":
-                self.login()
-            elif signedin.strip().upper() == "N":
-                self.register()
-            elif signedin.strip().lower() == 'exit':
-                self.stop = True
-                break
-            
-        while self.login == True:
-            print('\n')
-            print('- 1. Search for a food')
-            print('- 2. Show all foods')
-            print('- 3. Add a food')
-            print('- 4. Logout')
-            print('- 5. Exit')
-            choice = input('\nChoose an option: ')
-            print('\n')
-            if choice == '1':
-                self.search()
-            elif choice == '2':
-                self.show_all()
-            elif choice == '3':
-                self.add()
-            elif choice == '4':
-                self.login = False
-                print('\nLogged out')
-            elif choice == '5':
-                self.stop = True
-                print('\nBye')
-                break
-            else:
-                continue
+    def _connect(self): # Connect to the database
+        try:
+            conn = pymysql.connect(
+                host='localhost', 
+                user='root', 
+                password='mondayl#tm#brok#n', 
+                db='nutritional_values'
+                )
 
-    def login(self): # Login to the database
-        while True:
-            username = input('Username: ')
-            password = input('Password: ')
+            cursor = conn.cursor()
+            return conn, cursor
+        except pymysql.err.OperationalError:
+            print('An error has ocurred while connecting to the db')
 
-            if username.lower() == 'exit' or password.lower() == 'exit':
-                self.stop = True
-            
-            try:
-                self.connect()
-                request = f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'"
-                self.excecute(self.conn, request)
-                self.login = True
-                print(f'\nWelcome {username}')
-                self.conn.close()
-            except OperationalError:
-                print('\nWrong username or password\n')
-                continue
+    def run_request(self, request): # Execute a request
+        conn, cursor = self._connect()
+        result = cursor.execute(request)
+        conn.commit()
 
-    def register(self): # Register to the database
-        while True:
-            username = input('\n- Username: ')
-            password = input('- Password: ')
-            email = input('- Email: ')
-            age = input('- Age: ')
+        if result:
+            return result
 
-            self.connect()
-            request = f"INSERT INTO user (username, password, email, age) VALUES ('{username}', '{password}', '{email}', '{age}')"
-            self.excecute(self.conn, request)
-            request = f"INSERT INTO credentials (username, password) VALUES ('{username}', '{password}')"
-            self.excecute(self.conn, request)
-            print('\nUser created')
-            self.conn.close()
-            self.login = True
-            break
-    
     def show_all(self):
         self.connect()
         request = "SELECT * FROM calories"
@@ -102,56 +39,32 @@ class NutritionalValues():
             print(row)
         self.conn.close()
 
-    def add(self):pass
+    def visual(self):
+        lbl_food = Label(self.window, text='Food').place(x=20, y=15)
+        entry_food = Entry(self.window)
+        entry_food.place(x=75, y=17)
+        lbl_calories = Label(self.window, text='Calories').place(x=20, y=45)
+        entry_calories = Entry(self.window)
+        entry_calories.place(x=75, y=47)
+        add_button = Button(self.window, text='Add', command=lambda:self._add(
+            entry_food.get(), 
+            entry_calories.get()
+            ))
+        add_button.config(width=24)
+        add_button.place(x=21, y=87)
+    
+    def _add(self, food, calories):
+        if food != NoneType and calories != NoneType:
+            request = "INSERT INTO calories(name, calories) VALUES(%s, %s)"
+            conn, cursor = self._connect()
+            cursor.execute(request, (food, calories))
+            print('Sexesfully added!')
 
-    def visual_general(self):
-        root = Tk()
-        root.title('Nutritional Values')
-        canvas = Canvas(root)
-        
-        frame = Frame(root)
-        frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+root = Tk()
+root.title('Nutritional Values')
+root.geometry('900x500')
 
-        if self.login_status == False:
-            label = Label(frame, text="Do you have an account?")
-            label.grid(row=0, column=1)
+nv = NutritionalValues(root)
+nv.visual()
 
-            button1 = Button(frame, text="Yes", command=lambda:self.login())
-            button1.grid(row=1, column=1)
-            button2 = Button(frame, text="No", command=lambda:self.register())
-            button2.grid(row=1, column=2)
-        else:
-            while True:
-                label = Label(frame, text="- 1. Search for a food\n- 2. Show all foods\n- 3. Add a food\n- 4. Logout\n- 5. Exit")
-                label.grid(row=0, column=1)
-                entry_request = Entry(frame)
-                entry_request.grid(row=0, column=2)
-
-                if entry_request:
-                    if entry_request.get() == '1':
-                        self.search()
-                    elif entry_request.get() == '2':
-                        self.show_all()
-                    elif entry_request.get() == '3':
-                        self.add()
-                    elif entry_request.get() == '4':
-                        self.login = False
-                    elif entry_request.get() == '5':
-                        self.stop = True
-                    else:
-                        print('\nWrong input')
-                else:
-                    wrong_input_label = Label(frame, text="Wrong input")
-                    wrong_input_label.grid(row=1, column=1)
-                    continue
-
-if __name__ == "__main__": # Run the program
-    nv = NutritionalValues(True) # The instance is named as nv for short
-    nv.stop = False
-    while nv.stop != True:
-        try:
-            nv.run()
-        except KeyboardInterrupt:
-            nv.stop = True
-            print('\nBye')
-            
+root.mainloop()
